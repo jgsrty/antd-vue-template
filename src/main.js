544e6f22abcff151c733fddccd143ec1e83316ab
jgsrty <573850993@qq.com>
2019-02-18 15:41:50 +0800

    1.删除多余文件 2.测试button

--- src/main.js
+++ src/main.js
@@ -2,6 +2,10 @@ import Vue from 'vue'
 import App from './App.vue'
 import router from './router'
 import store from './store'
+import Antd from 'ant-design-vue'
+import 'ant-design-vue/dist/antd.css'
+
+Vue.use(Antd)
 
 Vue.config.productionTip = false
 
